8167189e2adb6440d99d820f1142b3415365ea72
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2024-10-14 16:07:37 +0700

    fix(server): change default port from 3001 to 3000

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import corsOptions from "./utils/cors.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? 3001;
+const port = process.env.PORT ?? 3000;
 
 app.use(cookieParser());
 app.use(express.json());
