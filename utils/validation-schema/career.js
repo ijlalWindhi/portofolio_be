3b50964a9e5008e7d60b7553d6d08ee4b3ccb6d6
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2024-10-15 10:57:44 +0700

    fix(validation): make end_date optional in career validation schema

--- utils/validation-schema/career.js
+++ utils/validation-schema/career.js
@@ -8,7 +8,7 @@ export const createCareerSchema = [
   body("location_type").isString().notEmpty().escape().trim(),
   body("type").isString().notEmpty().escape().trim(),
   body("start_date").isString().notEmpty().trim(),
-  body("end_date").isString().notEmpty().trim(),
+  body("end_date").isString().trim(),
   body("link").isString().notEmpty().trim(),
 ];
 
@@ -21,7 +21,7 @@ export const updateCareerSchema = [
   body("location_type").isString().notEmpty().escape().trim(),
   body("type").isString().notEmpty().escape().trim(),
   body("start_date").isString().notEmpty().trim(),
-  body("end_date").isString().notEmpty().trim(),
+  body("end_date").isString().trim(),
   body("link").isString().notEmpty().trim(),
 ];
 
