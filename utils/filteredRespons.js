63028732688ce2080854166c159d029f161ad827
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2024-10-17 11:47:04 +0700

    refactor(filteredResponse): enhance filtering logic to handle both arrays and single objects

--- utils/filteredRespons.js
+++ utils/filteredRespons.js
@@ -1,12 +1,19 @@
-export async function filteredResponse(response) {
-  try {
-    const newRespons = response.map((value) => {
-      const { id, createdAt, updatedAt, deletedAt, ...rest } = value;
-      return rest;
-    });
+export function filteredResponse(response) {
+  const filterObject = (obj) => {
+    const { id, createdAt, updatedAt, deletedAt, ...rest } = obj;
+    return rest;
+  };
 
-    return newRespons;
+  try {
+    if (Array.isArray(response)) {
+      return response.map(filterObject);
+    } else if (typeof response === "object" && response !== null) {
+      return filterObject(response);
+    } else {
+      return response;
+    }
   } catch (error) {
-    console.log(error);
+    console.error("Error in filteredResponse:", error);
+    return response;
   }
 }
